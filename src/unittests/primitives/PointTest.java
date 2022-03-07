6b6be755c11c120f6a1e665ebcb8b3d9df17e8ef
haiku <gorenchaim@gmail.com>
2022-03-07 17:09:11 +0200

    unittests

---
+++ src/unittests/primitives/PointTest.java
@@ -0,0 +1,24 @@
+package unittests.primitives;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PointTest {
+
+    @org.junit.jupiter.api.Test
+    void add() {
+    }
+
+    @Test
+    void testSubtract() {
+    }
+
+    @Test
+    void testDistanceSquared() {
+    }
+
+    @Test
+    void testDistance() {
+    }
+}
