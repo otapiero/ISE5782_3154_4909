7160e74ce656c00ced982bf535ef011a1d5eeabd
otapiero <oht862@gmail.com>
2022-04-12 18:02:52 +0300

    fixed assertCountIntersections

--- src/unittests/renderer/IntegrationTest.java
+++ src/unittests/renderer/IntegrationTest.java
@@ -12,21 +12,21 @@ class IntegrationTest {
         Camera cam1 = new Camera(
                 new Point(0,0,0),
                 new Vector(0, 0, -1),
-                new Vector(0, 1, 0));
+                new Vector(0, 1, 0)).setVPSize(3,3).setVPDistance(1);
         Camera cam2 = new Camera(
                 new Point(0, 0, 0.5),
                 new Vector(0, 0, -1),
-                new Vector(0, 1, 0));
+                new Vector(0, 1, 0)).setVPSize(3,3).setVPDistance(1);
         //TC:01 - Sphere in the middle of the screen
-        assertCountIntersections(cam1, new Sphere(new Point(0,0,-3), 1), 2);
+        assertCountIntersections(2, cam1, new Sphere(new Point(0,0,-3), 1),3,3);
         //TC:02 - screen is in the sphere
-        assertCountIntersections(cam2, new Sphere(new Point(0,0,-2.5), 2.5), 18);
+        assertCountIntersections(18, cam2, new Sphere(new Point(0,0,-2.5), 2.5),3,3);
         //TC:03 - medium sphere 10 points
-        assertCountIntersections(cam2, new Sphere( new Point(0, 0, -2),2), 10);
+        assertCountIntersections(10, cam2, new Sphere( new Point(0, 0, -2),2),3,3);
         //TC:04 -  screen and camera are in the sphere
-        assertCountIntersections(cam2, new Sphere( new Point(0, 0, -1),4), 9);
+        assertCountIntersections(9, cam2, new Sphere( new Point(0, 0, -1),4),3,3);
         //TC:05 -  sphere beyound the camera
-        assertCountIntersections(cam1, new Sphere( new Point(0, 0, 1),0.5), 0);
+        assertCountIntersections(0, cam1, new Sphere( new Point(0, 0, 1),0.5),3,3);
 
     }
     @Test
@@ -34,13 +34,13 @@ class IntegrationTest {
         Camera cam = new Camera(
                 new Point(0,0,0),
                 new Vector(0, 0, -1),
-                new Vector(0, 1, 0));
+                new Vector(0, 1, 0)).setVPSize(3,3).setVPDistance(1);
         //TC:01 - triangle in front of the screen 1 point
-        assertCountIntersections(cam, new Triangle(new Point(0,1,-2), new Point(1,-1,-2), new Point(-1,-1,-2)), 1);
+        assertCountIntersections(1, cam, new Triangle(new Point(0,1,-2), new Point(1,-1,-2), new Point(-1,-1,-2)),3,3);
         //TC:02 - triangle in front of the screen 2 points
-        assertCountIntersections(cam, new Triangle(new Point(0,20,-2), new Point(1,-1,-2), new Point(-1,-1,-2)), 2);
+        assertCountIntersections(2, cam, new Triangle(new Point(0,20,-2), new Point(1,-1,-2), new Point(-1,-1,-2)),3,3);
         //TC:03 - triangle behind the screen 0 points
-        assertCountIntersections(cam, new Triangle(new Point(0,1,5), new Point(1,-1,5), new Point(-1,-1,5)), 0);
+        assertCountIntersections(0, cam, new Triangle(new Point(0,1,5), new Point(1,-1,5), new Point(-1,-1,5)),3,3);
     }
     @Test
     void testPlaneIntergration() {
@@ -49,18 +49,18 @@ class IntegrationTest {
                 new Vector(0, 0, -1),
                 new Vector(0, 1, 0));
         //TC:01 - plane Parallel to the screen 9 points
-        assertCountIntersections(cam, new Plane(new Point(0,0,-2), new Vector(0,0,1)), 9);
+        assertCountIntersections(9, cam, new Plane(new Point(0,0,-2), new Vector(0,0,1)),3,3);
         //TC:02 - plane  9 points
-        assertCountIntersections(cam, new Plane(new Point(0, 0, -5), new Vector(0, 1, 2)), 9);
+        assertCountIntersections(9, cam, new Plane(new Point(0, 0, -5), new Vector(0, 1, 2)),3,3);
         //TC:03 - plane  6 points
-        assertCountIntersections(cam, new Plane(new Point(0, 0, -5), new Vector(0, 1, 1)), 6);
+        assertCountIntersections(6, cam, new Plane(new Point(0, 0, -5), new Vector(0, 1, 1)),3,3);
 
     }
-    private void assertCountIntersections(Camera cam, Intersectable geo, int expected) {
-        cam.setVPSize(3,3).setVPDistance(1);
 
-        int nX = 3, nY = 3, count = 0;
 
+
+    private void assertCountIntersections(int expected, Camera cam, Intersectable geo, int nX, int nY) {
+        int count = 0;
         for(int i = 0; i < nX; i++){
             for(int j = 0; j < nY; j++){
                 var ray = cam.constructRay(nX, nY, j, i); // create ray in the view plane
